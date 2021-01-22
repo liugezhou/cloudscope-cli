a39a9b708b4dc18fbb2bffb341bf8741cccfb632
liumingzhou <18231133236@163.com>
2021-01-22 14:06:19 +0800

    first commit

---
+++ packages/command/__tests__/command.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const command = require('..');
+
+describe('command', () => {
+    it('needs tests');
+});
---
+++ packages/command/lib/command.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = command;
+
+function command() {
+    // TODO
+}
