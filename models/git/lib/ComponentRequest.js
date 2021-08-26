48bec04ffc7230e4d5eae3fd9068af3d9bde0cdc
liumingzhou <18231133236@163.com>
2021-08-26 23:30:52 +0800

    add:Component upload to npm

--- models/git/lib/ComponentRequest.js
+++ models/git/lib/ComponentRequest.js
@@ -5,7 +5,6 @@ module.exports = {
   createComponent: async function(component) {
     try {
       const response = await axios.post('http://liugezhou.com:7001/api/v1/components', component);
-      log.verbose('response', response);
       const { data } = response;
       if (data.code === 0) {
         return data.data;
